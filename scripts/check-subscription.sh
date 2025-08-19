396ffc9921198d7dfbf4eefa79bb94563d43c4e5
Varanga de Silva <varanga.techno@gmail.com>
2025-08-19 13:28:38 +1000

    Fix subscription check and close if in infrastructure script

--- scripts/check-subscription.sh
+++ scripts/check-subscription.sh
@@ -24,12 +24,15 @@ then
     az account set -s "$ARM_SUBSCRIPTION_ID"
 fi
 
-export CURRENT_SUBSCRIPTION_ID=$(az account show --query id --output tsv)
-echo -e "Using subscription id $CURRENT_SUBSCRIPTION_ID"
+if ! CURRENT_SUBSCRIPTION_ID=$(az account show --query id --output tsv 2>/dev/null); then
+    echo -e "ERROR: Please run 'az login' to setup account."
+    exit 1
+fi
+export CURRENT_SUBSCRIPTION_ID
+echo -e "Using subscription id ${CURRENT_SUBSCRIPTION_ID}"
 
-# If the ARM_SUBSCRIPTION_ID is set, compare it with the 
-if [ -n "$ARM_SUBSCRIPTION_ID" ] && [ $CURRENT_SUBSCRIPTION_ID != "$ARM_SUBSCRIPTION_ID" ]
-then
+# If the ARM_SUBSCRIPTION_ID is set, compare it with the current subscription
+if [ -n "$ARM_SUBSCRIPTION_ID" ] && [ "$CURRENT_SUBSCRIPTION_ID" != "$ARM_SUBSCRIPTION_ID" ]; then
     echo -e "*** INCORRECT SUBSCRIPTION ***."
     echo -e "Either use subscription id $ARM_SUBSCRIPTION_ID, or unset the ARM_SUBSCRIPTION_ID environment variable in your .env"
     exit 1
